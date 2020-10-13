beta 1.03: Fix Bug, update the data scaling Function and optimize the mean value filter.

Function/Predicting/Prediction.py
@@ -8,27 +8,48 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import StandardScaler
 
-def InverseScaling(DP,Prediction):
+def InverseScaling(DP,Prediction,InverseLabel="y"):
+    """
+    Inverse Scale of the Target Dataset.
+    :param DP: [Data_Preperation], Original Dataset
+    :param Prediction: [ndarray], Target Dataset need to scale inversely.
+    :param InverseLabel:[Str], Use its mean and std. to scale inversely.
+    :return:
+    InversePrediction: [ndarray], Inverse Scale of Target Dataset.
+    """
     scaler=StandardScaler()
-    # Use Mean, Var from y_test to unscale
-    scaler.fit(DP.Merge_Dataset[DP.InverseLabel["y"]])
-    return scaler.inverse_transform(Prediction)
+
+    # Use Mean, Var from y_test to scale inversely
+    scaler.fit(DP.Merge_Dataset[DP.InverseLabel[InverseLabel]])
+    InversePrediction= scaler.inverse_transform(Prediction)
+    return InversePrediction
 
 def Filter(Prediction, window=40, order=1):
+    """
+    Implementation of Mean Value Filter for Prediction.
+    :param Prediction: [ndarray], Prediction need to be filtered.
+    :param window: [int], Window size for Filter to compute the mean value.
+    :param order: [int], Order of filter, implement the filter "order" times(Recursion).
+    :return:
+    Target_Result: [ndarray], Prediction after filtering.
+    """
     shape=(Prediction.shape[0]-window,Prediction.shape[1])
     Filted_Result=np.empty(shape)
     for i in range(Prediction.shape[1]):
         for j in range(0,Prediction.shape[0]-window):
             Filted_Result[j][i]=statistics.mean(Prediction[j:j+window,i])
 
-    return Filted_Result
+    Target_Result=Filted_Result
 
+    # Recursion
+    if order>1:
+        Target_Result=Filter(Target_Result,window,order-1)
 
+    return Target_Result
 
-
-def Predicting(Regressor,DP,Time_Domain=False,Channel="Overview"):
+def Predicting(Regressor,DP,Time_Domain=False,Channel="Overview",FilterOrder=1):
     """
-    Visualize the prediction.
+    Predicting and Visualize the prediction.
     :param Regressor: [MLP Regressor], Initial or Optimized MLP Regressor.
     :param DP: [Data_Preperation], Data Set after Pre Processing.
     :param Time_Domain: [boolean], True: Predicting in Time Domain. False: Predicting the Training Result.
@@ -39,93 +60,144 @@ def Predicting(Regressor,DP,Time_Domain=False,Channel="Overview"):
     #Use Project Data
     if Time_Domain==False:
         Prediction = Regressor.predict(DP.Scaled_Dataset[DP.InverseLabel["X_test"]])
-        Prediction = InverseScaling(DP,Prediction)
-        Target = DP.Merge_Dataset[DP.InverseLabel["y_test"]]
+        # Prediction = InverseScaling(DP,Prediction)
+        Target = DP.Scaled_Dataset[DP.InverseLabel["y_test"]]
+        Assignment = AssignChannel(Target,Prediction)
+        ShowMae(Assignment)
+        Plot(Assignment,Channel)
     # Use Customer Data
     else:
         Prediction=Regressor.predict(DP.Scaled_Dataset[DP.InverseLabel["X"]])
-        Prediction = Filter(Prediction)
-        # Prediction = Filter(Prediction)
-        Prediction = InverseScaling(DP, Prediction)
-        # Prediction = Filter(Prediction)
-        Target = DP.Merge_Dataset[DP.InverseLabel["y"]]
+        # Prediction = InverseScaling(DP, Prediction)
+        Target = DP.Scaled_Dataset[DP.InverseLabel["y"]]
+        Prediction_F = Filter(Prediction, order=FilterOrder)
+        Assignment = AssignChannel(Target, Prediction_F)
+        Plot(Assignment,Channel)
+        return Prediction
 
+def AssignChannel(Target,Prediction):
+    """
+    Assign the Output channel and extract the data from Prediction for analysing.
+    :param Target: [ndarray], Value of Target Dataset.
+    :param Prediction: [ndarray], Value of Prediction Dataset.
+    :return:
+    dict: [Dict], Dict of ndarray(Data Column), {m2, m3, m4, k, alpha, beta, Time_Domain,
+    m2_T, m3_T, m4_T, k_T, alpha_T, beta_T, Time_Domain_F}
+    """
     # Assign Output Channel(Target)
-    m2_T=Target[:,0]
-    m3_T=Target[:,1]
-    m4_T=Target[:,2]
-    k_T=Target[:,3]
-    alpha_T=Target[:,4]
-    beta_T=Target[:,5]
-    Time_Domain=Target.shape[0]
+    dict = {}
+    dict["m2_T"]=Target[:,0]
+    dict["m3_T"]=Target[:,1]
+    dict["m4_T"]=Target[:,2]
+    dict["k_T"]=Target[:,3]
+    dict["alpha_T"]=Target[:,4]
+    dict["beta_T"]=Target[:,5]
+    dict["Time_Domain"]=Target.shape[0]
     print(Prediction.shape)
 
     # Assign Output Channel(Prediction)
-    m2=Prediction[:,0]
-    m3=Prediction[:,1]
-    m4=Prediction[:,2]
-    k=Prediction[:,3]
-    alpha=Prediction[:,4]
-    beta=Prediction[:,5]
-    Time_Domain_F=Prediction.shape[0]
+    dict["m2"]=Prediction[:,0]
+    dict["m3"]=Prediction[:,1]
+    dict["m4"]=Prediction[:,2]
+    dict["k"]=Prediction[:,3]
+    dict["alpha"]=Prediction[:,4]
+    dict["beta"]=Prediction[:,5]
+    dict["Time_Domain_F"]=Prediction.shape[0]
+
+    return dict
 
     # Visualization
+def ShowMae(Assignment):
+    """
+    Visualize the MAE and NAME of each channel.
+    :param Assignment: [dict], Dictionary of Output Channel and Target Channel.
+    :return:
+    void
+    """
+    mae = mean_absolute_error(Assignment["m2_T"], Assignment["m2"])
+    print("m2 Mean Absolute Error:", mae)
+    print("m2 Normalized Mean Absolute Error in %:", 100 * mae / np.ptp(Assignment["m2_T"]))
+    mae = mean_absolute_error(Assignment["m3_T"], Assignment["m3"])
+    print("m3 Mean Absolute Error:", mae)
+    print("m3 Normalized Mean Absolute Error in %:", 100 * mae / np.ptp(Assignment["m3_T"]))
+    mae = mean_absolute_error(Assignment["m4_T"], Assignment["m4"])
+    print("m4 Mean Absolute Error:", mae)
+    print("m4 Normalized Mean Absolute Error in %:", 100 * mae / np.ptp(Assignment["m4_T"]))
+    mae = mean_absolute_error(Assignment["k_T"],Assignment["k"])
+    print("k Mean Absolute Error:", mae)
+    print("k Normalized Mean Absolute Error in %:", 100 * mae / np.ptp(Assignment["k_T"]))
+    mae = mean_absolute_error(Assignment["alpha_T"], Assignment["alpha"])
+    print("alpha Mean Absolute Error:", mae)
+    print("alpha Normalized Mean Absolute Error in %:", 100 * mae / np.ptp(Assignment["alpha_T"]))
+    mae = mean_absolute_error(Assignment["beta_T"], Assignment["beta"])
+    print("beta Mean Absolute Error:", mae)
+    print("beta Normalized Mean Absolute Error in %:", 100 * mae / np.ptp(Assignment["beta_T"]))
+
+def Plot(Assignment,Channel="Overview"):
+    """
+    Plot the scatter diagramm for training and predicting.
+    :param Assignment: [dict], Dictionary of Output Channel and Target Channel.
+    :param Channel: [Str], Overview or choose the specific channel to show details.
+    :return:
+    void.
+    """
     if Channel=="Overview":
         plt.subplot(321)
-        plt.scatter(range(Time_Domain_F),m2,marker="x",color="blue")
-        plt.scatter(range(Time_Domain),m2_T,marker="o",color="red",alpha=0.6)
+        plt.scatter(range(Assignment["Time_Domain_F"]),Assignment["m2"],marker="x",color="blue")
+        plt.scatter(range(Assignment["Time_Domain"]),Assignment["m2_T"],marker="o",color="red",alpha=0.6)
         plt.title("m2")
         plt.subplot(322)
-        plt.scatter(range(Time_Domain_F),m3,marker="x",color="blue")
-        plt.scatter(range(Time_Domain),m3_T,marker="o",color="red",alpha=0.6)
+        plt.scatter(range(Assignment["Time_Domain_F"]),Assignment["m3"],marker="x",color="blue")
+        plt.scatter(range(Assignment["Time_Domain"]),Assignment["m3_T"],marker="o",color="red",alpha=0.6)
         plt.title("m3")
         plt.subplot(323)
-        plt.scatter(range(Time_Domain_F), m4, marker="x", color="blue")
-        plt.scatter(range(Time_Domain), m4_T, marker="o", color="red",alpha=0.6)
+        plt.scatter(range(Assignment["Time_Domain_F"]), Assignment["m4"], marker="x", color="blue")
+        plt.scatter(range(Assignment["Time_Domain"]), Assignment["m4_T"], marker="o", color="red",alpha=0.6)
         plt.title("m4")
         plt.subplot(324)
-        plt.scatter(range(Time_Domain_F), k, marker="x", color="blue")
-        plt.scatter(range(Time_Domain), k_T, marker="o", color="red",alpha=0.6)
+        plt.scatter(range(Assignment["Time_Domain_F"]), Assignment["k"], marker="x", color="blue")
+        plt.scatter(range(Assignment["Time_Domain"]), Assignment["k_T"], marker="o", color="red",alpha=0.6)
         plt.title("k")
         plt.subplot(325)
-        plt.scatter(range(Time_Domain_F),alpha,marker="x",color="blue")
-        plt.scatter(range(Time_Domain), alpha_T, marker="o", color="red",alpha=0.6)
+        plt.scatter(range(Assignment["Time_Domain_F"]),Assignment["alpha"],marker="x",color="blue")
+        plt.scatter(range(Assignment["Time_Domain"]), Assignment["alpha_T"], marker="o", color="red",alpha=0.6)
         plt.title("alpha")
         plt.subplot(326)
-        plt.scatter(range(Time_Domain_F),beta,marker="x",color="blue")
-        plt.scatter(range(Time_Domain), beta_T, marker="o", color="red",alpha=0.6)
+        plt.scatter(range(Assignment["Time_Domain_F"]),Assignment["beta"],marker="x",color="blue")
+        plt.scatter(range(Assignment["Time_Domain"]), Assignment["beta_T"], marker="o", color="red",alpha=0.6)
         plt.title("beta")
         plt.show()
 
+
     else:
         if Channel=="m2":
-            plt.scatter(range(Time_Domain_F),m2,label="Prediction",marker="x",color="blue")
-            plt.scatter(range(Time_Domain),m2_T,label="Target",marker="o",color="red",alpha=0.6)
+            plt.scatter(range(Assignment["Time_Domain_F"]),Assignment["m2"],label="Prediction",marker="x",color="blue")
+            plt.scatter(range(Assignment["Time_Domain"]),Assignment["m2_T"],label="Target",marker="o",color="red",alpha=0.6)
 
 
         if Channel=="m3":
-            plt.scatter(range(Time_Domain_F), m4, label="Prediction", marker="x", color="blue")
-            plt.scatter(range(Time_Domain), m4_T, label="Target", marker="o", color="red",alpha=0.6)
+            plt.scatter(range(Assignment["Time_Domain_F"]), Assignment["m3"], label="Prediction", marker="x", color="blue")
+            plt.scatter(range(Assignment["Time_Domain"]), Assignment["m3_T"], label="Target", marker="o", color="red",alpha=0.6)
 
 
         if Channel=="m4":
-            plt.scatter(range(Time_Domain_F), m4, label="Prediction",marker="x", color="blue")
-            plt.scatter(range(Time_Domain), m4_T, label="Target",marker="o", color="red", alpha=0.6)
+            plt.scatter(range(Assignment["Time_Domain_F"]), Assignment["m4"], label="Prediction",marker="x", color="blue")
+            plt.scatter(range(Assignment["Time_Domain"]), Assignment["m4_T"], label="Target",marker="o", color="red", alpha=0.6)
 
 
         if Channel=="k":
-            plt.scatter(range(Time_Domain_F), k, label="Prediction",marker="x", color="blue")
-            plt.scatter(range(Time_Domain), k_T, label="Target",marker="o", color="red", alpha=0.6)
+            plt.scatter(range(Assignment["Time_Domain_F"]), Assignment["k"], label="Prediction",marker="x", color="blue")
+            plt.scatter(range(Assignment["Time_Domain"]), Assignment["k_T"], label="Target",marker="o", color="red", alpha=0.6)
 
 
         if Channel=="alpha":
-            plt.scatter(range(Time_Domain_F), alpha, label="Prediction",marker="x", color="blue")
-            plt.scatter(range(Time_Domain), alpha_T, label="Target",marker="o", color="red", alpha=0.6)
+            plt.scatter(range(Assignment["Time_Domain_F"]), Assignment["alpha"], label="Prediction",marker="x", color="blue")
+            plt.scatter(range(Assignment["Time_Domain"]), Assignment["alpha_T"], label="Target",marker="o", color="red", alpha=0.6)
 
 
         if Channel=="beta":
-            plt.scatter(range(Time_Domain_F), beta, marker="x", label="Prediction",color="blue")
-            plt.scatter(range(Time_Domain), beta_T, marker="o", label="Target",color="red", alpha=0.6)
+            plt.scatter(range(Assignment["Time_Domain_F"]), Assignment["beta"], marker="x", label="Prediction",color="blue")
+            plt.scatter(range(Assignment["Time_Domain"]), Assignment["beta_T"], marker="o", label="Target",color="red", alpha=0.6)
 
 
         plt.title(Channel)

Function/Preprocessing/DataPreperation.py
@@ -1,22 +1,29 @@
 import numpy as np
+import joblib
 import sys
 from Data import DataReader as DR
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from matplotlib import pyplot as plt
 class Data_Preperation:
-    def __init__(self, *data_set,mode="Training"):
+    def __init__(self, mode,*,data_set,DP=None):
         self.input_set=data_set[0]
         self.target_set=data_set[1]
         self.Label={}
         self.InverseLabel = {"X_train": 0, "y_train": 1, "X_test": 2, "y_test": 3,"X":0,"y":1}
         self.Merge_Dataset=[]
-        self.Split_Dataset=[]
         self.Scaled_Dataset=[]
         self.input_feature=self.input_set[0].shape[1]
         self.target_feature=self.target_set[0].shape[1]
+        self.scaler=StandardScaler()
+
+        if mode=="Training":
+            self.Split_Dataset = []
+            self.Merge_Split_Dataset = []
+            self.scaler.fit(self.MergeData()[0])
+
         if mode=="Predicting":
-            pass
+            self.scaler=DP.scaler
 
     def AddParameter(self,L,number):
         for i in range(number):
@@ -60,13 +67,18 @@ class Data_Preperation:
 
     def MergeData(self):
         """
-        Merge the Dataset(for Predicting)
+        Merge the Data set.
         :return:
-        Merge_Dataset: [List], [input, target]
+        Merge_Dataset: [List], [X, y].
+        Merge_Dataset[0]: [ndarray], Merged Input set.
+        Merge_Dataset[1]: [ndarray],Merged Target set
         """
-        Merge_Dataset=["",""]
-        Merge_Dataset[0]=self.input_set[0]
-        Merge_Dataset[1]=self.target_set[0]
+        self.Label={0:"X",1:"y"}
+        Merge_Dataset=[self.input_set[0],self.target_set[0]]
+        for i in range(1,len(self.input_set)):
+            Merge_Dataset[0]=np.append(Merge_Dataset[0],self.input_set[i],axis=0)
+            Merge_Dataset[1]=np.append(Merge_Dataset[1],self.target_set[i],axis=0)
+        print(type(Merge_Dataset))
         self.Merge_Dataset=Merge_Dataset
         return Merge_Dataset
 
@@ -74,60 +86,93 @@ class Data_Preperation:
         """
         Merge the already split Dataset(for Training)
         :return:
-        Merge_Dataset: [List], [X_train, y_train, X_test, y_test, X_del, y_del].
-        Merge_Dataset[0]: [nparray], merged Training Input Set
-        Merge_Dataset[1]: [nparray], merged Training Target Set
-        Merge_Dataset[2]: [nparray], merged Test Input Set
-        Merge_Dataset[3]: [nparray], merged Test Target Set
-        (Merge_Dataset[4]: [nparray], merged Development Input Set
-        Merge_Dataset[5]: [nparray], merged Development Target Set)
+        Merge_Split_Dataset: [List], [X_train, y_train, X_test, y_test, X_del, y_del].
+        Merge_Split_Dataset[0]: [ndarray], merged Training Input Set
+        Merge_Split_Dataset[1]: [ndarray], merged Training Target Set
+        Merge_Split_Dataset[2]: [ndarray], merged Test Input Set
+        Merge_Split_Dataset[3]: [ndarray], merged Test Target Set
+        (Merge_Split_Dataset[4]: [ndarray], merged Development Input Set
+        Merge_Split_Dataset[5]: [ndarray], merged Development Target Set)
         """
-        Merge_Dataset=[]
+        Merge_Split_Dataset=[]
         for i in range(len(self.Split_Dataset)):
-            Merge_Dataset.append(self.Split_Dataset[i][0])
+            Merge_Split_Dataset.append(self.Split_Dataset[i][0])
             if not len(self.Split_Dataset[0])==1:
                 for j in range(len(self.Split_Dataset[i])):
                     if j>0:
-                        Merge_Dataset[i]=np.append(Merge_Dataset[i], self.Split_Dataset[i][j],axis=0)
-        self.Merge_Dataset=Merge_Dataset
-        return Merge_Dataset
+                        Merge_Split_Dataset[i]=np.append(Merge_Split_Dataset[i], self.Split_Dataset[i][j],axis=0)
+        self.Merge_Split_Dataset=Merge_Split_Dataset
+        return Merge_Split_Dataset
 
-    def DataScaling(self,Merge_Dataset,Mean=False,Var=False):
+    def DataScaling(self,Merge_Dataset,Load_Model=False,Unify=True,Path=None,Mean=False,Var=False):
 
         """
         Use Mean Value and Standard Deviation of features to scale the Merged Data Set(for Training / Predicting).
-        :param Merge_Dataset: [List], For Training: [X_train, y_train, X_test, y_test, X_del, y_del]
-                                      For Predicting: [input, target]
+        :param Merge_Dataset: [List], For Training: Merge_Split_Dataset, [X_train, y_train, X_test, y_test, X_del, y_del]
+                                      For Predicting: Merge_Dataset, [input, target]
+        :param Load_Model: [boolean], When True, Input the existed scaler from external .joblib file.
+        :param Unify: [boolean], Use unified Mean and Std.(True) or seperate Mean and Std.(False) to scale the Dataset.
+        :param Path: [Str], Path of the existed scaler.
         :param Mean: [boolean], show Mean value of each feature when "True".
         :param Var:  [boolean], shown Variance of each feature when "True".
         :return:
         Scaled_Dataset for Training: [X_train, y_train, X_test, y_test, X_del, y_del]
-                       for Predicting: [input, target]
-        Scaled_Dataset[0]: [nparray], Scaled Training Input Set/input Set
-        Scaled_Dataset[1]: [nparray], Scaled Training Target Set/target Set
-        Scaled_Dataset[2]: [nparray], Scaled Test Input Set
-        Scaled_Dataset[3]: [nparray], Scaled Test Target Set
-        (Scaled_Dataset[4]: [nparray], Scaled Development Input Set
-        Scaled_Dataset[5]: [nparray], Scaled Development Target Set)
+                       for Predicting: [X, y]
+        Scaled_Dataset[0]: [ndarray], Scaled Training Input Set/input Set
+        Scaled_Dataset[1]: [ndarray], Scaled Training Target Set/target Set
+        Scaled_Dataset[2]: [ndarray], Scaled Test Input Set
+        Scaled_Dataset[3]: [ndarray], Scaled Test Target Set
+        (Scaled_Dataset[4]: [ndarray], Scaled Development Input Set
+        Scaled_Dataset[5]: [ndarray], Scaled Development Target Set)
         """
+        Scaled_Dataset = []
 
-        scaler=StandardScaler()
-        Scaled_Dataset=[]
-        for i in range(len(Merge_Dataset)):
-            Scaled_Dataset.append("")
-            Scaled_Dataset[i]=scaler.fit_transform(Merge_Dataset[i])
-            if Mean==True:
-                print(f"Mean value of {self.Label[i]}: {scaler.mean_}")
-            if Var==True:
-                print(f"Standard Deviation of {self.Label[i]}. Set is {scaler.var_}")
+        # Perform Scaling
+        if Load_Model==False:
+            scaler=StandardScaler()
+            if Unify==False:
+                print("Use different Scaler!")
+            else:
+                print("Use Unified Scaler!")
+                if Mean==True:
+                    print(f"Mean value of Unified Data:{self.scaler.mean_}")
+                if Var==True:
+                    print(f"Variation of Unified Data:{self.scaler.var_}")
+
+            for i in range(len(Merge_Dataset)):
+                Scaled_Dataset.append("")
+
+                # Scale the input feature
+                if i%2==0:
+
+                    # Only Scale the input feature with different Mean and Std
+                    if Unify==False:
+                        Scaled_Dataset[i]=scaler.fit_transform(Merge_Dataset[i])
+                        if Mean == True:
+                            print(f"Mean value of {self.Label[i]}: {scaler.mean_}")
+                        if Var == True:
+                            print(f"Variation of {self.Label[i]}. Set is {scaler.var_}")
+
+                    # Scale the input feature with unified Mean and Std
+                    else:
+                        Scaled_Dataset[i] = self.scaler.transform(Merge_Dataset[i])
+
+                else:
+                    Scaled_Dataset[i]=Merge_Dataset[i]
+
+        # Scale with existed Scaler
+        else:
+            scaler=joblib.load(Path)
+            for i in range(len(Merge_Dataset)):
+                Scaled_Dataset.append("")
+                if i%2==0:
+                    Scaled_Dataset[i]=scaler.transform(Merge_Dataset[i])
+                else:
+                    Scaled_Dataset[i]=Merge_Dataset[i]
+            print("Use existed scaler, mean value:\n",scaler.mean_)
+
         self.Scaled_Dataset=Scaled_Dataset
-        # a=scaler.transform(Scaled_Dataset[5], copy=True)
-        # plt.scatter(range(Scaled_Dataset[5].shape[0]),Scaled_Dataset[5][:,1])
-        # a=scaler.inverse_transform(Scaled_Dataset[5])
-        # print()
-        # plt.scatter(range(Scaled_Dataset[5].shape[0]),a[:,1],marker="x", color="blue")
-        # plt.scatter(range(Scaled_Dataset[5].shape[0]),Merge_Dataset[5][:,1],marker="o", color="red")
-        # plt.show()
+
         return Scaled_Dataset
 
 